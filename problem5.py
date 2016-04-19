13474949659179ffdba2400012292ce84c886122
Anders Kromann <anders_kromann@hotmail.com>
2016-04-19 18:12:25 +0200

    fixed 5

--- problem5.py
+++ problem5.py
@@ -1,9 +1,9 @@
 def isDivisible(num):
-    for i in range(1,20):
+    for i in range(20, 1, -1):
         if num % i != 0:
             return False
     return True
 
-for i in range(1,100000000):
-    if isDivisible(i):
-        print i
+for i in range(1,50000000):
+    if isDivisible(i * 20):
+        print i * 20
